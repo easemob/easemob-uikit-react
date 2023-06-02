c95c156d4d93ba445316678d7262e8b78f1a6a5e
myZhangDong <973849793@qq.com>
2023-06-02 10:35:42 +0800

    feat: text message supports URL preview

--- module/textMessage/TextMessage.tsx
+++ module/textMessage/TextMessage.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import Avatar from '../../src/avatar';
 import MessageStatus, { MessageStatusProps } from '../messageStatus/MessageStatus';
@@ -9,7 +9,9 @@ import { getConversationTime } from '../utils';
 import BaseMessage, { BaseMessageProps } from '../baseMessage';
 import rootStore from '../store/index';
 import type { TextMessageType } from '../types/messageType';
-
+import { getLinkPreview, getPreviewFromContent } from 'link-preview-js';
+import { UrlMessage } from './UrlMessage';
+import reactStringReplace from 'react-string-replace';
 export interface TextMessageProps extends BaseMessageProps {
   textMessage: TextMessageType;
   // color?: string; // 字体颜色
@@ -22,7 +24,8 @@ export interface TextMessageProps extends BaseMessageProps {
   style?: React.CSSProperties;
 }
 
-const renderTxt = (txt: string | undefined | null) => {
+const renderTxt = (txt: string | undefined | null, detectedUrl: string | undefined) => {
+  const urlRegex = /(https?:\/\/\S+)/gi;
   if (txt === undefined || txt === null) {
     return [];
   }
@@ -56,9 +59,59 @@ const renderTxt = (txt: string | undefined | null) => {
     start = index + match[1].length;
   }
   rnTxt.push(txt.substring(start, txt.length));
+  // if (detectedUrl) {
+  rnTxt.forEach((text, index) => {
+    if (urlRegex.test(text!.toString())) {
+      let replacedText = reactStringReplace(text?.toString() || '', urlRegex, (match, i) => (
+        <a key={match + i} target="_blank" href={match} className="message-text-url-link">
+          {match}
+        </a>
+      ));
+      rnTxt[index] = replacedText;
+    }
+  });
+  // }
 
   return rnTxt;
 };
+
+const REGEX_VALID_URL = new RegExp(
+  '^' +
+    // protocol identifier
+    '(?:(?:https?|ftp)://)' +
+    // user:pass authentication
+    '(?:\\S+(?::\\S*)?@)?' +
+    '(?:' +
+    // IP address exclusion
+    // private & local networks
+    '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
+    '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
+    '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
+    // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broacast addresses
+    // (first & last IP address of each class)
+    '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
+    '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
+    '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
+    '|' +
+    // host name
+    '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)' +
+    // domain name
+    '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*' +
+    // TLD identifier
+    '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))' +
+    // TLD may end with dot
+    '\\.?' +
+    ')' +
+    // port number
+    '(?::\\d{2,5})?' +
+    // resource path
+    '(?:[/?#]\\S*)?' +
+    '$',
+  'i',
+);
 
 export const TextMessage = (props: TextMessageProps) => {
   let {
@@ -72,6 +125,8 @@ export const TextMessage = (props: TextMessageProps) => {
   } = props;
   const { getPrefixCls } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('message-text', customizePrefixCls);
+  const [urlData, setUrlData] = useState<any>(null);
+  const [isFetching, setFetching] = useState(false);
   let { bySelf, time, from, msg } = textMessage;
 
   const classString = classNames(prefixCls, className);
@@ -82,6 +137,37 @@ export const TextMessage = (props: TextMessageProps) => {
   if (!type) {
     type = bySelf ? 'primary' : 'secondly';
   }
+  console.log('渲染文本组件');
+  const urlPreview = useRef<any>(null);
+
+  const detectedUrl = msg
+    ?.replace(/\n/g, ' ')
+    .split(' ')
+    .find(function (token) {
+      return REGEX_VALID_URL.test(token);
+    });
+  if (detectedUrl) {
+    console.log(detectedUrl);
+  }
+  useEffect(() => {
+    if (detectedUrl) {
+      setFetching(true);
+      getLinkPreview(msg)
+        .then(data => {
+          urlPreview.current = data;
+          setFetching(false);
+          return setUrlData(data);
+        })
+        .catch(e => {
+          setFetching(false);
+          console.log(e);
+        });
+    }
+  }, [detectedUrl]);
+  let urlTxtClass = '';
+  if (urlData?.images.length > 0) {
+    urlTxtClass = 'message-text-hasImage';
+  }
   return (
     <BaseMessage
       direction={bySelf ? 'rtl' : 'ltr'}
@@ -89,9 +175,11 @@ export const TextMessage = (props: TextMessageProps) => {
       time={time}
       nickName={nickName || from}
       bubbleType={type}
+      className={urlTxtClass}
       {...others}
     >
-      <span className={classString}>{renderTxt(msg)}</span>
+      <span className={classString}>{renderTxt(msg, detectedUrl)}</span>
+      {!!detectedUrl && <UrlMessage {...urlData} isLoading={isFetching}></UrlMessage>}
     </BaseMessage>
   );
 };
---
+++ module/textMessage/UrlMessage.tsx
@@ -0,0 +1,49 @@
+import React, { memo, ReactNode, useEffect } from 'react';
+import classNames from 'classnames';
+import Avatar from '../../src/avatar';
+import MessageStatus, { MessageStatusProps } from '../messageStatus/MessageStatus';
+import { ConfigContext } from '../../src/config/index';
+import './style/style.scss';
+
+export interface UrlMessageProps {
+  mediaType: 'website' | 'image' | 'application';
+  url: string;
+  title?: string;
+  description?: string;
+  favicons?: string[];
+  images?: string[];
+  isLoading: boolean;
+}
+
+const UrlMessage2 = (props: UrlMessageProps) => {
+  const { mediaType, url, title = '', description = '', favicons, images, isLoading } = props;
+
+  const Loading = () => {
+    return <div className="message-text-url-loading">parsing...</div>;
+  };
+
+  let content: JSX.Element | undefined;
+  if (!mediaType) return null;
+  if (mediaType === 'website') {
+    let logo = images?.[0];
+    content = (
+      <>
+        <div>{logo && <img src={logo} alt="image" className="message-text-url-img" />}</div>
+        <div className="message-text-url-info">
+          <div className="message-text-url-title">{title}</div>
+          <div className="message-text-url-desc">{description}</div>
+        </div>
+      </>
+    );
+  }
+
+  if (isLoading) {
+    content = <Loading></Loading>;
+  }
+  return <div className="message-text-url">{content}</div>;
+};
+
+const UrlMessage = memo(UrlMessage2, (p, n) => {
+  return p.url == n.url;
+});
+export { UrlMessage };
