86aecdb20d5502759dfe3399597569b6c9cf0c27
myZhangDong <973849793@qq.com>
2023-11-08 14:47:19 +0800

    feat: update emoji reaction

--- module/textMessage/TextMessage.tsx
+++ module/textMessage/TextMessage.tsx
@@ -48,7 +48,8 @@ export const renderTxt = (txt: string | undefined | null, parseUrl: boolean = tr
   }
   let rnTxt: React.ReactNode[] = [];
   let match;
-  const regex = /(\[.*?\])/g;
+  const regex =
+    /(U\+1F600|U\+1F604|U\+1F609|U\+1F62E|U\+1F92A|U\+1F60E|U\+1F971|U\+1F974|U\+263A|U\+1F641|U\+1F62D|U\+1F610|U\+1F607|U\+1F62C|U\+1F913|U\+1F633|U\+1F973|U\+1F620|U\+1F644|U\+1F910|U\+1F97A|U\+1F928|U\+1F62B|U\+1F637|U\+1F912|U\+1F631|U\+1F618|U\+1F60D|U\+1F922|U\+1F47F|U\+1F92C|U\+1F621|U\+1F44D|U\+1F44E|U\+1F44F|U\+1F64C|U\+1F91D|U\+1F64F|U\+2764|U\+1F494|U\+1F495|U\+1F4A9|U\+1F48B|U\+2600|U\+1F31C|U\+1F308|U\+2B50|U\+1F31F|U\+1F389|U\+1F490|U\+1F382|U\+1F381)/g;
   let start = 0;
   let index = 0;
   while ((match = regex.exec(txt))) {
