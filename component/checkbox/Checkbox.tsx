95e130142fb065fb343606852fa82961b7298054
myZhangDong <973849793@qq.com>
2024-07-17 18:30:18 +0800

    feat: Checkbox supports ref prop

--- component/checkbox/Checkbox.tsx
+++ component/checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ReactElement, useState } from 'react';
+import React, { ChangeEvent, ReactElement, useState, forwardRef } from 'react';
 import classNames from 'classnames';
 import { ConfigContext } from '../config/index';
 import './style/style.scss';
@@ -16,57 +16,62 @@ export interface CheckboxProps {
   shape?: 'square' | 'ground';
 }
 
-const Checkbox = ({
-  id,
-  checked,
-  defaultChecked,
-  disabled = false,
-  children,
-  className,
-  onChange,
-  shape = 'ground',
-  prefix,
-  style = {},
-}: CheckboxProps): ReactElement => {
-  const { getPrefixCls } = React.useContext(ConfigContext);
-  const prefixCls = getPrefixCls('checkbox', prefix);
-  const classes = classNames(
-    prefixCls,
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  (
     {
-      [`${prefixCls}-ground`]: shape == 'ground',
-    },
-    className,
-  );
-  const [isChecked, setIsCheck] = useState(checked);
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(event);
-  };
-  let checkedInner = defaultChecked || isChecked;
-  if (checked != undefined) {
-    checkedInner = checked;
-  }
-
-  return (
-    <label className={`${prefixCls}-wrapper`} style={{ ...style }} htmlFor={id}>
-      <span className={classes}>
-        <input
-          disabled={disabled}
-          id={id}
-          type="checkbox"
-          checked={checkedInner}
-          onClick={() => {
-            if (typeof checked != undefined) {
-              return;
-            }
-            if (!disabled) setIsCheck(!isChecked);
-          }}
-          onChange={handleChange}
-        />
-        <span className={classNames(`${prefixCls}--mark`, disabled ? 'disabled' : '')} />
-      </span>
-      {children !== undefined && <span>{children}</span>}
-    </label>
-  );
-};
-
+      id,
+      checked,
+      defaultChecked = false,
+      disabled = false,
+      children,
+      className,
+      onChange,
+      shape = 'ground',
+      prefix,
+      style = {},
+    }: CheckboxProps,
+    ref,
+  ): ReactElement => {
+    const { getPrefixCls } = React.useContext(ConfigContext);
+    const prefixCls = getPrefixCls('checkbox', prefix);
+    const classes = classNames(
+      prefixCls,
+      {
+        [`${prefixCls}-ground`]: shape == 'ground',
+      },
+      className,
+    );
+    const [isChecked, setIsCheck] = useState(checked || false);
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(event);
+    };
+    let checkedInner = defaultChecked || isChecked;
+    if (checked != undefined) {
+      checkedInner = checked;
+    }
+    return (
+      <label className={`${prefixCls}-wrapper`} style={{ ...style }} htmlFor={id}>
+        <span className={classes}>
+          <input
+            ref={ref}
+            disabled={disabled}
+            id={id}
+            type="checkbox"
+            checked={checkedInner}
+            onClick={() => {
+              if (typeof checked != 'undefined') {
+                return;
+              }
+              if (!disabled) setIsCheck(!isChecked);
+            }}
+            onChange={handleChange}
+          />
+          <span className={classNames(`${prefixCls}--mark`, disabled ? 'disabled' : '')} />
+        </span>
+        {children !== undefined && <span>{children}</span>}
+      </label>
+    );
+  },
+);
+Checkbox.displayName = 'Checkbox';
 export { Checkbox };
